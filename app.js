db6e38df0ede4800335a885664f64f16d2738c11
cyrusmew <51240567+Cyrusroy001@users.noreply.github.com>
2022-06-05 19:20:05 +0530

    refactor date.js and make layout using ejs

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-let items = [];
-let workItems = [];
+
+// we are varying this but still const!!, because JS 
+// allows pushing but not reassigning
+const items = [];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    // working with date formats
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US", options);
-
+    const day = date.getDate();
     // rendering the template ejs file
     res.render("list", {
         listTitle: day,
@@ -27,7 +23,7 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
@@ -43,6 +39,10 @@ app.get("/work", function (req, res) {
         listTitle: "Work List",
         newItems: workItems,
     });
+});
+
+app.get("/about", function (req, res) {
+    res.render("about");
 });
 
 app.listen(3000, function () {
